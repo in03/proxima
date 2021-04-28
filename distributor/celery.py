8094086e953a05fd8c4697b933389de3926b9ef4
Caleb Trevatt <in03@users.noreply.github.com>
2021-04-28 11:25:39 +1000

    Initial

---
+++ distributor/celery.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from celery import Celery
+
+import os
+import yaml
+
+script_dir = os.path.dirname(__file__)
+with open(os.path.join(script_dir, "proxy_settings.yml")) as file: 
+    config = yaml.safe_load(file)
+
+# Broker Settings
+broker = config['rabbit_mq']
+user = broker['user']
+pwd = broker['password']
+address = broker['address']
+vhost = broker['vhost']
+protocol = broker['protocol']
+backend = broker['backend']
+
+
+
+app = Celery('proxy_encoder',
+             broker=f'{protocol}://{user}:{pwd}@{address}/{vhost}',
+             backend=backend,
+             include=['proxy_encoder.tasks'])
---
+++ run_celery.bat
@@ -0,0 +1,1 @@
+celery -A distributor worker --loglevel=info
