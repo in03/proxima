d634cd5ad50ef56624ddddfe0c802d0a9d340435
Caleb Trevatt <in03@users.noreply.github.com>
2021-12-09 08:43:17 +1000

    Add versioning detection when not package

--- resolve_proxy_encoder/helpers.py
+++ resolve_proxy_encoder/helpers.py
@@ -1,4 +1,5 @@
 """ Helper functions for main module """
+from asyncio import subprocess
 import json
 import logging
 import sys
@@ -6,7 +7,7 @@ import time
 from typing import Union
 
 import pkg_resources
-import semver
+import subprocess
 from rich.logging import RichHandler
 from rich.prompt import Prompt
 from win10toast import ToastNotifier
@@ -16,39 +17,38 @@ from resolve_proxy_encoder.settings import app_settings
 
 config = app_settings.get_user_settings()
 
-def get_rich_logger(loglevel:Union[int, str]="WARNING"):
 
+def get_rich_logger(loglevel: Union[int, str] = "WARNING"):
 
-    """ Set logger to rich, allowing for console markup."""
+    """Set logger to rich, allowing for console markup."""
 
     FORMAT = "%(message)s"
     logging.basicConfig(
-
-        level = loglevel, 
-        format = FORMAT, 
-        datefmt = "[%X]", 
-        handlers = [
-
+        level=loglevel,
+        format=FORMAT,
+        datefmt="[%X]",
+        handlers=[
             RichHandler(
-                rich_tracebacks = True,
-                tracebacks_extra_lines = 1,
-                markup = True,
+                rich_tracebacks=True,
+                tracebacks_extra_lines=1,
+                markup=True,
             )
         ],
     )
     logger = logging.getLogger("rich")
     return logger
 
-def app_exit(level:int=0, timeout:int=5, cleanup_funcs:list=None):
 
-    """ 
-    Exit function to allow time to 
+def app_exit(level: int = 0, timeout: int = 5, cleanup_funcs: list = None):
+
+    """
+    Exit function to allow time to
     read console output if necessary before exit.
     Specify negative timeout to prompt exit instead of timeout.
     Provide a list of functions to call on cleanup if necessary.
     """
 
-    logger = get_rich_logger(config['loglevel'])
+    logger = get_rich_logger(config["loglevel"])
 
     # Run any cleanup functions
     if cleanup_funcs:
@@ -58,7 +58,7 @@ def app_exit(level:int=0, timeout:int=5, cleanup_funcs:list=None):
         for x in cleanup_funcs:
 
             if x is not None:
-                
+
                 logger.debug(f"Running: {x}")
                 x()
 
@@ -73,23 +73,25 @@ def app_exit(level:int=0, timeout:int=5, cleanup_funcs:list=None):
             sys.stdout.write(f"\rExiting in " + str(i))
 
         # Erase last line
-        sys.stdout.write('\x1b[1A')
-        sys.stdout.write('\x1b[2K')
+        sys.stdout.write("\x1b[1A")
+        sys.stdout.write("\x1b[2K")
 
     sys.exit(level)
 
-def toast(message, threaded = True):
+
+def toast(message, threaded=True):
     toaster = ToastNotifier()
     toaster.show_toast(
-        "Queue Proxies", 
-        message, 
-        # icon_path = icon_path, 
-        threaded = threaded,
+        "Queue Proxies",
+        message,
+        # icon_path = icon_path,
+        threaded=threaded,
     )
     return
 
+
 def get_resolve_objects():
-    """ Return necessary Resolve objects with error handling"""
+    """Return necessary Resolve objects with error handling"""
 
     logger = get_rich_logger()
 
@@ -99,8 +101,9 @@ def get_resolve_objects():
 
     except:
 
-        logger.warning("[red] :warning: Couldn't access the Resolve Python API. Is DaVinci Resolve running?[/]", 
-            extra = {"markup":True}
+        logger.warning(
+            "[red] :warning: Couldn't access the Resolve Python API. Is DaVinci Resolve running?[/]",
+            extra={"markup": True},
         )
         app_exit(1, -1)
 
@@ -112,7 +115,7 @@ def get_resolve_objects():
 
         logger.warning(
             "[red] :warning: Couldn't get current project. Is a project open in Resolve?[/]",
-            extra = {"markup":True}
+            extra={"markup": True},
         )
         app_exit(1, -1)
 
@@ -124,7 +127,7 @@ def get_resolve_objects():
 
         logger.warning(
             "[red] :warning: Couldn't get current timeline. Is a timeline open in Resolve?[/]",
-            extra = {"markup":True}
+            extra={"markup": True},
         )
         app_exit(1, -1)
 
@@ -136,21 +139,21 @@ def get_resolve_objects():
 
         logger.warning(
             "[red] :warning: Couldn't get Resolve's media pool.[/]",
-            extra = {"markup":True}
+            extra={"markup": True},
         )
         app_exit(1, -1)
-        
 
     return dict(
-        resolve = resolve, 
-        project = project,
-        timeline = timeline,
-        media_pool = media_pool,
+        resolve=resolve,
+        project=project,
+        timeline=timeline,
+        media_pool=media_pool,
     )
 
+
 def get_package_last_commit(package_name):
-    """ Get the ID of the last VCS commit pushed when this package was installed.
-    
+    """Get the ID of the last VCS commit pushed when this package was installed.
+
     Uses `direct_url.json` from the site-package's `dist-info`.
 
     Args:
@@ -161,30 +164,32 @@ def get_package_last_commit(package_name):
         None
 
     Raises:
-        none: 
+        none:
     """
-
-    dist = pkg_resources.get_distribution(package_name)
 
     try:
 
+        print("Getting commit ID from package dist info.")
+
+        dist = pkg_resources.get_distribution(package_name)
         vcs_metadata_file = dist.get_metadata("direct_url.json")
         vcs_metadata = json.loads(vcs_metadata_file)
-        package_last_commit_id = vcs_metadata['vcs_info']['commit_id']
+        package_last_commit_id = vcs_metadata["vcs_info"]["commit_id"]
 
-    except KeyError:
-        print("Couldn't get commit_id from 'direct_url.json'")
+        return package_last_commit_id
+
+    except:
+        print("Couldn't get package dist info, assuming git repo.")
+        print("Getting commit ID from git")
+
+        output = subprocess.check_output(
+            "git rev-parse HEAD", stderr=subprocess.STDOUT, shell=True
+        )
+
+        if output:
+            package_last_commit_id = output.decode()
+        else:
+            print(f"Couldn't get git info!\n" + "Check properly cloned or installed")
+            app_exit(1, -1)
 
     return package_last_commit_id
-
-# def get_package_semver(package_name):
-#     """ Get and parse a package's semver if exists.
-
-#     Args:
-#         package_name (str): the name of the package
-    
-#     Returns:
-#     """
-
-#     dist = pkg_resources.get_distribution(package_name)
-#     semver.VersionInfo.parse(dist)
