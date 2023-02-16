2ed0c6c585d5fb986624b2c5c2126eca4a65d809
in03 <in03@users.noreply.github.com>
2023-02-16 16:04:32 +1000

    refactor: Remove unused import

--- src/proxima/settings/__init__.py
+++ src/proxima/settings/__init__.py
@@ -4,5 +4,3 @@ settings_dir = Path(__file__).parent
 default_settings_file = str(Path(settings_dir, "default_settings.toml").absolute())
 user_settings_file = str(Path(settings_dir, "user_settings.toml"))
 dotenv_settings_file = str(Path(settings_dir, ".env").absolute())
-
-# from .manager import settings
