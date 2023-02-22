1b15cb1a57c6f6a8663a32511a5fd69f2c11e653
in03 <in03@users.noreply.github.com>
2023-02-22 10:30:12 +1000

    fix(settings): Updates overwrite user_settings.toml

--- src/proxima/settings/__init__.py
+++ src/proxima/settings/__init__.py
@@ -1,6 +1,13 @@
+import os
+import shutil
 from pathlib import Path
+from rich import print
 
 settings_dir = Path(__file__).parent
 default_settings_file = str(Path(settings_dir, "default_settings.toml").absolute())
 user_settings_file = str(Path(settings_dir, "user_settings.toml"))
 dotenv_settings_file = str(Path(settings_dir, ".env").absolute())
+
+if not os.path.exists(user_settings_file):
+    print("[magenta][Initialising 'user_settings.toml']")
+    shutil.copy(default_settings_file, user_settings_file)
